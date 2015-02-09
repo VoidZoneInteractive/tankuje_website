91e579c11c36bc7327fc4285f0b6d44e9fe0625e
Void Zone <developer+github@voidzone.pl>
2015-02-10 00:40:45 +0100

    Some bugfixes

--- application/controller/webservice.php
+++ application/controller/webservice.php
@@ -5,9 +5,19 @@ class Controller_Webservice extends Controller
     public function action_default()
     {
         $params = array(
-            'limit' => 20,
+//            'limit' => 20,
         );
         $stations = Dao_Station::select($params);
+
+	$companies = Dao_Company::select(array('index_by' => 'company_id'));
+
+exit(print_r($companies,1));
+
+        foreach ($stations as &$station)
+        {
+            $station['lat'] = (float)$station['lat'];
+            $station['lng'] = (float)$station['lng'];
+        }
 
         exit(json_encode($stations));
     }
